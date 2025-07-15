fe4475bfbbbfa1720b6689851486a2ac9abd10cc
jorr3 <84990635+jorr3@users.noreply.github.com>
2025-07-15 18:58:01 +0200

    Remove redundant comments GroversAlgorithm.java

--- src/main/java/quantum/algorithms/GroversAlgorithm.java
+++ src/main/java/quantum/algorithms/GroversAlgorithm.java
@@ -12,18 +12,14 @@ public class GroversAlgorithm {
         int n = register.getNumQubits();
         QuantumCircuit circuit = new QuantumCircuit(register);
 
-        // Step 2: Create superposition
         for (int i = 0; i < n; i++) {
             circuit.addGate(new HadamardGate(), i);
         }
 
-        // Step 3 and 4: Repeat Oracle and Amplification
         int iterations = (int) Math.ceil(Math.sqrt(Math.pow(2, n)));
         for (int i = 0; i < iterations; i++) {
-            // Apply Oracle
             circuit.addGate(oracle);
 
-            // Grover's Diffuser
             for (int j = 0; j < n; j++) {
                 circuit.addGate(new HadamardGate(), j);
                 circuit.addGate(new PauliXGate(), j);
@@ -35,7 +31,6 @@ public class GroversAlgorithm {
             }
         }
 
-        // Step 6: Measurement
         for (int i = 0; i < n; i++) {
             circuit.addMeasurement(i);
         }
